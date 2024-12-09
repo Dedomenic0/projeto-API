2f78d46aa40999397cfd0080ec47a70508071275
Dedomenic0 <eduardo.dedomenico@gmail.com>
2024-12-09 08:24:32 -0300

    livraria

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import express from "express";
+import conectaNaDataBase from "./config/dbConect.js";
+import routes from "./routes/index.js";
+
+
+const conexao = await conectaNaDataBase();
+
+conexao.on("error", (erro) => {
+    console.error("Erro de conexão", erro);
+});
+
+conexao.once("open", () => {
+    console.log("Conectado ao banco de dados")
+})
+
+const app = express();
+routes(app);
+
+export default app;
+
+
